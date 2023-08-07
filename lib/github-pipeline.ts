e80a99552c63043ce86ef45cc3ab1608f068d0ce
Minhao Zhang <davidzmh@gmail.com>
2023-08-06 23:57:27 -0700

    add connection arn

--- lib/github-pipeline.ts
+++ lib/github-pipeline.ts
@@ -10,7 +10,8 @@ export class GithubPipelineStack extends cdk.Stack {
     const pipeline = new CodePipeline(this, 'Pipeline', {
       pipelineName: 'GithubPipeline',
       synth: new ShellStep('Synth', {
-        input: CodePipelineSource.gitHub('zhang356/almond-services', 'main'),
+        input: CodePipelineSource.connection('zhang356/almond-services', 'main', {
+            connectionArn: 'arn:aws:codestar-connections:us-west-2:309550911295:connection/13d964fa-ac45-4fa7-bbd2-1c1db060f2c0' }),
         commands: ['npm ci', 'npm run build', 'npx cdk synth']
       })
     });
